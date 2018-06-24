3e030eb673e1bdebf10c521174ae8864ff106db4
OITtanigawa <e1b15064@oit.ac.jp>
2018-06-24 23:35:33 +0900

    改良

--- TestBot.py
+++ TestBot.py
@@ -17,10 +17,29 @@ async def on_message(message):
 		)
 		player = voice.create_ffmpeg_player('test.mp3')
 		player.start()
+		while True:
+			if(player.is_done() != True):
+				print ("playing music...")
+			else:
+				player.stop()
+				break
+		print("music end")
+		for x in client.voice_clients:
+			return await x.disconnect()
+
+	if message.content.startswith("本田"):
+		voice = await client.join_voice_channel(
+			client.get_channel("429186094827962368")
+		)
+		player = voice.create_ffmpeg_player('honda.mp3')
+		player.start()
 		while player.is_done() != True:
 			print ("playing music...")
 		print("music end")
 		print("end")
 		for x in client.voice_clients:
 			return await x.disconnect()
+
+	player.stop()
+
 client.run("NDYwMzkyMDA0MzkyODQ1MzIz.DhEJWg.Xopeu55-O6_uirZi8bbX5qtDc34")
